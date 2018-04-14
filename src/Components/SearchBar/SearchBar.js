a7c5a226bb246de14bf4ec400d990c86602899b2
danielmcintosh1 <daniel@cardinaladvising.com>
2018-04-14 09:02:09 -0700

    add search logic

--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -2,10 +2,32 @@ import React, { Component } from 'react';
 import './SearchBar.css';
 
 class SearchBar extends Component {
+  constructor(props){
+    super(props);
+    this.search = this.search.bind(this);
+    this.handleTermChange = this.handleTermChange.bind(this);
+  }
+
+  search(term) {
+    this.props.onSearch(term)
+  }
+
+  handleTermChange(termChangeEvent) {
+    const term = termChangeEvent.target.value;
+    this.setState({term: term});
+  }
+
+  handleNameChange(onChangeEvent) {
+    const name = onChangeEvent.target.value;
+    this.props.onNameChange(name);
+  }
+
   render() {
     return (
       <div className="SearchBar">
-        <input placeholder="Enter A Song, Album, or Artist" />
+        <input
+        placeholder="Enter A Song, Album, or Artist"
+        onChange = {this.handleTermChange} />
         <a>SEARCH</a>
       </div>
     );
